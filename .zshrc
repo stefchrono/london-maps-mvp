c23826cf361885faa2a2289b995e5bfa9721c17e
Stef Chrono <stefchrono@gmail.com>
2020-07-29 09:13:21 +0100

    Updated Settings

--- .zshrc
+++ .zshrc
@@ -1,2 +1,4 @@
 export TFL_API_ID="03315ccd"
 export TFL_API_KEY="20be96920a9a9de7d6f63b72056e69a1"
+export SECRET_KEY="fcf97cf04a1d8bf2a0d8ddf26fddd256c2d5a9e22b145081"
+export DEBUG_VALUE="True"
--- Maps/settings.py
+++ Maps/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&7i8$@(b!%3fc2mj^5565x^avxg@!@v_yfc-_ked%$f9746-74'
+# SECRET_KEY = '&7i8$@(b!%3fc2mj^5565x^avxg@!@v_yfc-_ked%$f9746-74'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['londonmaps.herokuapp.com']
 
 
 # Application definition
