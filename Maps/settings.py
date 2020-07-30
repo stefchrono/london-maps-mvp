5f84546574ccf3d8832af72339b46479e91c817d
Stef Chrono <stefchrono@gmail.com>
2020-07-30 09:55:36 +0100

    Debug False

--- Maps/settings.py
+++ Maps/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&7i8$@(b!%3fc2mj^5565x^avxg@!@v_yfc-_ked%$f9746-74'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = False
 
 ALLOWED_HOSTS = ['londonmaps.herokuapp.com', '127.0.0.1']
 
